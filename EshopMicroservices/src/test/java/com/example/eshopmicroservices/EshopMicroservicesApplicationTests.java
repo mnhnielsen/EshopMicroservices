215ce537df1d40623df8b8435d9dd7d5e80cc246
Frederik <frederik@efio.dk>
2023-10-26 09:49:41 +0200

    new structure of projects

---
+++ EshopMicroservices/src/test/java/com/example/eshopmicroservices/EshopMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eshopmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
