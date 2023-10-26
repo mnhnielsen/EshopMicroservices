12e23c7f8e318c94b537a3f83ba7c1e945886f36
Frederik <frederik@efio.dk>
2023-10-26 11:16:32 +0200

    updated structure

--- EshopMicroservices/product_service/src/main/java/dk/sdu/product_service/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.product_service.repository;
-
-import dk.sdu.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
