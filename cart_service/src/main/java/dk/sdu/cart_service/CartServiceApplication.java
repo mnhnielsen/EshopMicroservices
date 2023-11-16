f26cf13567b90e051bb4ad1f14fb5e1d42328d51
oskar566 <oschr20@student.sdu.dk>
2023-11-16 14:26:38 +0100

    Created api-gateway

--- cart_service/src/main/java/dk/sdu/cart_service/CartServiceApplication.java
+++ cart_service/src/main/java/dk/sdu/cart_service/CartServiceApplication.java
@@ -1,7 +1,9 @@
 package dk.sdu.cart_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CartServiceApplication {
