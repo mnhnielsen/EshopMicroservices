daa5d45e348d83dd74ffd367e0d435e79140f739
oskar566 <oschr20@student.sdu.dk>
2023-12-21 15:25:14 +0100

    Added order-service to eureka server
    Had to change spring-boot-starter-parent to version 3.1.5 from 3.2.0. Do not know if this will cause any troubles with the order service functionality, have not tested. But it registers with the discovery server now.

--- order-service/src/main/java/dk/sdu/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/dk/sdu/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package dk.sdu.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
